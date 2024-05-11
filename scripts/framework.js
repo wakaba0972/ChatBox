57125d9d78d9a9994f3429e2d10fed7b556f298c
wakaba0972 <benny.wu2199@gmail.com>
2024-05-12 00:54:58 +0800

    username

--- scripts/framework.js
+++ scripts/framework.js
@@ -10,8 +10,6 @@ var v = new Vue({
             this.name = localStorage.getItem('name')
         }
 
-        if(this.name == null) this.name = "user" + Number(Math.random().toString() + Date.now()).toString(16).substr(2)
-
         window.addEventListener('keypress', (key) => {
             if(key.which == 13){
                 this.send()
